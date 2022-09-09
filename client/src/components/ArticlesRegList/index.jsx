b570066051442807566f6f9af0fff33c5b91262f
yevhen <yevhenfc@gmail.com>
2022-09-09 15:43:25 +0300

    ArticlesRegList add articles

--- client/src/components/ArticlesRegList/index.jsx
+++ client/src/components/ArticlesRegList/index.jsx
@@ -5,11 +5,35 @@ import CONSTANTS from '../../constants';
 
 function ArticlesRegList() {
   return (
-    <div className='styles.articlesMainContainer'>
-      
-    </div>
-  )
-}
+    <div className={styles.articlesMainContainer}>
+      {
+        articles.map((column, i) =>(
+          <div key={i} className={styles.ColumnContainer}> 
+            {column.map((data, j) => (
+              <div key={j}>
+                <div className={styles.headerArticle}>{data.head}</div>
+                <div className={styles.article}>      {data.body}</div>    
+              </div>
+            ))}
+            {
+              <>
+              <div className={styles.headerArticle}>
+                I have other questions! How can I get in touch with Squadhelp?
+              </div>
+              <div className={styles.article}>
+                Check out our
+                {' '}<span className={styles.orangeSpan}>FAQs</span>{' '} 
+                or send us a
+                {' '}<span className={styles.orangeSpan}>message</span>
+                . For assistance with launching a contest, you can also call us at {CONSTANTS.CONTACTS.TEL} or schedule a
+                {' '}<span className={styles.orangeSpan}>Branding Consultation</span>
+              </div>
+              </>
+            }
+          </div>
+      ))}
+    </div>   
+  )}
 
 export default  ArticlesRegList;
 
